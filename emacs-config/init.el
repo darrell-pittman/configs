7ac10f3b3457bb5a517227a7669248b2febf4a6f
Darrell Pittman <darrell.pittman@fonemed.com>
2018-01-03 14:34:40 -0330

    New init function for phms

--- emacs-config/init.el
+++ emacs-config/init.el
@@ -22,3 +22,18 @@
 
 ;;; init.el ends here
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dumb-jump cider expand-region undo-tree magit flycheck counsel-projectile projectile linum-relative multiple-cursors company counsel ace-window org-bullets which-key try solarized-theme use-package)))
+ '(safe-local-variable-values (quote ((projectile-project-name . "rails")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
