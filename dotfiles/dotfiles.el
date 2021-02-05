2522a5523de1a3a75ff9364b84af64575bc29576
Darrell Pittman <darrellppittman@gmail.com>
2021-02-05 10:11:48 -0330

    Some clean-up of dotfiles.el

--- dotfiles/dotfiles.el
+++ dotfiles/dotfiles.el
@@ -1,19 +1,26 @@
-(setq dotfiles-folder"~/configs/emacs-config")
-(setq dotfiles-org-files '("emacs.org" "emacs-private.org"))
+;;; Package -- Summary
+;;; Commentary:
+;;; Code:
+
+(require 'org)
+
+(defconst dotfiles-folder "~/configs/emacs-config" "Folder containing dotfiles.")
+(defconst dotfiles-org-files '("emacs.org" "emacs-private.org") "List of dotfiles.")
 
 (defun dotfiles-tangle-org-file (&optional org-file)
-  "Tangles ORG-FILE"
+  "Tangles ORG-FILE."
   (interactive "F")
-  (message "Tangling File: %s" org-file)
+  (message "Tangling Org File: %s" org-file)
   (let ((org-confirm-babel-evaluate nil)
 	(message-log-max nil)
 	(inhibit-message t))
     (org-babel-tangle-file (expand-file-name org-file dotfiles-folder))))
 
 (defun dotfiles-tangle-org-files ()
-  "Tangle all dotfiles."
+  "Tangle all dotfiles specified in dotfiles-org-files."
   (interactive)
   (dolist (org-file dotfiles-org-files)
     (dotfiles-tangle-org-file org-file))
   (message "Dotfiles are up to date!"))
 
+;;; dotfiles.el ends here
